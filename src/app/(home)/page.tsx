cf902d16980d08ff7fe1ddc8459a14831dc9e06a
TryastsinEugene <eugenetryas@gmail.com>
2025-09-18 01:08:43 +0300

    Added tRPC

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,20 @@
+"server-only"
 
-export default function Home() {
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function Home() {
+  void trpc.hello.prefetch({text: "Antonio"});
+
   return (
-    <div>
-      Some Content
-    </div>
+    <HydrateClient> 
+      <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <PageClient />
+      </ErrorBoundary>
+      </Suspense>
+    </HydrateClient> 
   );
 }
